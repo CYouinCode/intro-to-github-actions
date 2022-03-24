1485cade6027d49b08ab4f9125d28311fab1a55b
CYouinCode <101191636+CYouinCode@users.noreply.github.com>
2022-03-24 20:07:50 +0800

    changed message to hi

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hi link', () => {
+test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/hi/i);
   expect(linkElement).toBeInTheDocument();
